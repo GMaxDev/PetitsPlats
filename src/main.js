41eaee505878770d69a934f284c2b58badc639df
gmaxdev <maxime.guigra@gmail.com>
2023-12-05 13:53:49 +0100

    feat fillter: add buttons with total recipe

--- src/main.js
+++ src/main.js
@@ -1,13 +1,3 @@
 import "../style.css";
 
-document.querySelector("#app").innerHTML = `
-  <div class="bg-red-500 flex flex-col gap-4 max-w-xl m-auto hover:bg-blue-500">
-    <h1>Hello Vite!</h1>
-    <div class="card">
-      <button id="counter" type="button"></button>
-    </div>
-    <p class="read-the-docs">
-      Click on the Vite logo to learn more
-    </p>
-  </div>
-`;
+
