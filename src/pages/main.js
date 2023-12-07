e3f994e1fbb63a6d3f17707c8904152578d22404
gmaxdev <maxime.guigra@gmail.com>
2023-12-07 11:11:18 +0100

    feat recipeCard full completed

--- src/pages/main.js
+++ src/pages/main.js
@@ -11,18 +11,28 @@ function createRecipeCard(recipeInstances) {
   recipeInstances.forEach((recipeInstance) => {
     const recipeCard = document.createElement("div");
     const recipeZone = document.createElement("div");
+    const recipteTime = document.createElement("p");
     const ingredientZone = document.createElement("div");
-    const h3 = document.createElement("h4");
+    const h3 = document.createElement("h3");
     const h4Recipe = document.createElement("h4");
     const h4Ingredients = document.createElement("h4");
-    const ingredientName = document.createElement("h5");
     const p = document.createElement("p");
     const img = document.createElement("img");
 
     // const cheminImage = `assets/img/`;
 
-    recipeCard.setAttribute("class", "h-full bg-white rounded-3xl");
-    recipeZone.setAttribute("class", "mt-6 mb-14 mx-6");
+    recipeCard.setAttribute(
+      "class",
+      "recipeCard h-full bg-white rounded-3xl relative"
+    );
+
+    recipteTime.innerHTML = recipeInstance.time + `min`;
+    recipteTime.setAttribute(
+      "class",
+      "recipeTime px-4 py-1.5 absolute top-5 right-5 text-xs rounded-xl bg-amber-300"
+    );
+
+    recipeZone.setAttribute("class", "recipeZone mt-6 mb-14 mx-6");
 
     img.src = `../../assets/img/${recipeInstance.image}`;
     img.setAttribute("class", "w-full h-64 object-cover rounded-t-3xl");
@@ -31,19 +41,34 @@ function createRecipeCard(recipeInstances) {
     h3.setAttribute("class", "font-bold text-lg mb-7");
 
     h4Recipe.innerHTML = "Recette";
-    h4Recipe.setAttribute("class", "mb-3.5");
+    h4Recipe.setAttribute(
+      "class",
+      "text-xs uppercase  text-neutral-500 font-bold"
+    );
 
     p.innerHTML = recipeInstance.description;
-    p.setAttribute("class", "mb-8");
+    p.setAttribute(
+      "class",
+      "text-sm mt-5 h-20 w-full overflow-hidden text-ellipsis"
+    );
 
     h4Ingredients.innerHTML = "Ingrédients";
-    h4Ingredients.setAttribute("class", "mb-4");
+    h4Ingredients.setAttribute(
+      "class",
+      "text-xs uppercase mt-4 text-neutral-500 font-bold"
+    );
+
+    ingredientZone.setAttribute(
+      "class",
+      "grid grid-cols-2 mt-4 ingredientZone"
+    );
 
     // -----------------------------------------------
 
     recipeCardZone.appendChild(recipeCard);
 
     recipeCard.appendChild(img);
+    recipeCard.appendChild(recipteTime);
     recipeCard.appendChild(recipeZone);
 
     recipeZone.appendChild(h3);
@@ -54,7 +79,34 @@ function createRecipeCard(recipeInstances) {
 
     if (recipeInstance.ingredients) {
       recipeInstance.ingredients.forEach((ingredient) => {
-        ingredientName.innerHTML = ingredient.name;
+        const ingredientBloc = document.createElement("div");
+        const ingredientName = document.createElement("p");
+        const ingredientQuantity = document.createElement("p");
+
+        ingredientBloc.setAttribute("class", "ingredientBloc h-16");
+        ingredientZone.appendChild(ingredientBloc);
+
+        ingredientName.innerHTML = ingredient.ingredient;
+        ingredientName.setAttribute(
+          "class",
+          "ingredientName text-sm font-medium"
+        );
+        ingredientBloc.appendChild(ingredientName);
+
+        if (ingredient.quantity !== undefined) {
+          ingredientQuantity.innerHTML = ingredient.quantity;
+          ingredientQuantity.setAttribute(
+            "class",
+            "ingredientQuantity text-neutral-500 text-sm"
+          );
+
+          if (ingredient.unit) {
+            ingredientQuantity.innerHTML += ingredient.unit;
+          }
+        } else {
+          ingredientQuantity.innerHTML = "-";
+        }
+        ingredientBloc.appendChild(ingredientQuantity);
       });
     }
   });
