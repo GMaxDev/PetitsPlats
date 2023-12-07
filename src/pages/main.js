0df02069dbd46bf74db50c598c253c9add34dcee
gmaxdev <maxime.guigra@gmail.com>
2023-12-08 00:24:46 +0100

    feat function displayActiveFilter

--- src/pages/main.js
+++ src/pages/main.js
@@ -4,32 +4,61 @@ import { ModelRecipe } from "../models/ModelRecipe";
 import { createRecipeCard } from "../utils/createRecipeCard";
 import { totalRecipeUpdate } from "../utils/totalRecipeUpdate";
 
-const inputSearch = document.getElementById('search');
+const inputSearch = document.getElementById("search");
 const recipeInstances = dataRecipes.map(
   (recipeData) => new ModelRecipe(recipeData)
 );
+let filterList = [];
 
 createRecipeCard(recipeInstances);
 totalRecipeUpdate(recipeInstances);
 
-inputSearch.addEventListener('input', (event) => {
-  const valueInput = event.target.value
-  const recipeCards = document.getElementsByClassName("recipeCard")
-  let totalMatchRecipe = 0
+inputSearch.addEventListener("input", (event) => {
+  const valueInput = event.target.value;
+  const recipeCards = document.getElementsByClassName("recipeCard");
+  let totalMatchRecipe = 0;
 
-  for(let i=0; i<recipeInstances.length; i++ ){
+  for (let i = 0; i < recipeInstances.length; i++) {
     const currentRecipeCard = recipeCards[i];
-    const textContent = currentRecipeCard.id.toLowerCase()
+    const textContent = currentRecipeCard.id.toLowerCase();
 
     if (!textContent.includes(valueInput.toLowerCase())) {
-      recipeCards[i].style.display = 'none'
+      recipeCards[i].style.display = "none";
     } else {
-      recipeCards[i].style.display = 'block'
-      totalMatchRecipe++
+      recipeCards[i].style.display = "block";
+      totalMatchRecipe++;
     }
   }
   totalRecipeUpdate(totalMatchRecipe);
-})
+});
 
-function filterSearchBar(){
+inputSearch.addEventListener("keydown", (event) => {
+  const valueInput = event.target.value;
+  if (event.key === "Enter") {
+    event.preventDefault();
+
+    const valeurActuelle = valueInput.trim();
+    if (valeurActuelle !== "") {
+      filterList.push(valeurActuelle);
+    }
+
+    displayActiveFilter(filterList);
+    inputSearch.value = "";
+  }
+});
+
+function displayActiveFilter(array) {
+  const filterSpecificationZone = document.getElementById("filterSpecificationZone");
+  filterSpecificationZone.innerText = ''
+
+  array.forEach((element) => {
+    const elementBloc = document.createElement("p");
+    elementBloc.innerHTML = `${element} <i class="fa-solid fa-xmark"></i>`;
+    elementBloc.setAttribute(
+      "class",
+      "flex justify-between items-center h-14 w-52 mt-7 p-4 font-medium rounded-lg bg-amber-300 filterData"
+    );
+
+    filterSpecificationZone.appendChild(elementBloc);
+  });
 }
