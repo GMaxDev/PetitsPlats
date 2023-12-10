eee45bb15cb8a5682952386475ea5564dd076e3d
gmaxdev <maxime.guigra@gmail.com>
2023-12-10 13:40:24 +0100

    avancement des bloc de filtre

--- src/pages/main.js
+++ src/pages/main.js
@@ -48,3 +48,27 @@ inputSearch.addEventListener("keydown", (event) => {
     console.log(filterList)
   }
 });
+
+function dropdownFilter(data){
+  const dropdownIngredients = document.getElementById("ingredientList")
+  const dropdownAppliance = document.getElementById("applianceList")
+  const dropdownUstensils = document.getElementById("ustensilsList")
+
+  data.forEach(element => {
+    element.ingredients.forEach(ingredient => {
+      const ingredientitemLi = document.createElement("li")
+      ingredientitemLi.innerHTML = ingredient.ingredient
+      ingredientitemLi.setAttribute("class", "pl-4 py-2 cursor-pointer")
+      dropdownIngredients.append(ingredientitemLi)
+    })
+
+    const applianceitemLi = document.createElement("li")
+
+    element.ustensils.forEach(ustensil => {
+      const ustensilsitemLi = document.createElement("li")
+    })
+
+  });
+}
+
+dropdownFilter(recipeInstances)
