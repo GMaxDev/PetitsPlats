b6d372eb13edf059ba1e0ef2b999a06ee247e6b0
gmaxdev <maxime.guigra@gmail.com>
2023-12-10 21:43:35 +0100

    déplacement du filtre sélectionné en haut de la liste + suppression de son ancienne position

--- src/pages/main.js
+++ src/pages/main.js
@@ -4,6 +4,7 @@ import { ModelRecipe } from "../models/ModelRecipe";
 import { createRecipeCard } from "../utils/createRecipeCard";
 import { totalRecipeUpdate } from "../utils/totalRecipeUpdate";
 import { displayActiveFilter } from "../utils/displayActiveFilter";
+import { dropdownFilter } from "../utils/dropdownFilter";
 
 const inputSearch = document.getElementById("search");
 const recipeInstances = dataRecipes.map(
@@ -48,27 +49,5 @@ inputSearch.addEventListener("keydown", (event) => {
     console.log(filterList)
   }
 });
-
-function dropdownFilter(data){
-  const dropdownIngredients = document.getElementById("ingredientList")
-  const dropdownAppliance = document.getElementById("applianceList")
-  const dropdownUstensils = document.getElementById("ustensilsList")
-
-  data.forEach(element => {
-    element.ingredients.forEach(ingredient => {
-      const ingredientitemLi = document.createElement("li")
-      ingredientitemLi.innerHTML = ingredient.ingredient
-      ingredientitemLi.setAttribute("class", "pl-4 py-2 cursor-pointer")
-      dropdownIngredients.append(ingredientitemLi)
-    })
-
-    const applianceitemLi = document.createElement("li")
-
-    element.ustensils.forEach(ustensil => {
-      const ustensilsitemLi = document.createElement("li")
-    })
-
-  });
-}
 
 dropdownFilter(recipeInstances)
---
+++ src/utils/dropdownFilter.js
@@ -0,0 +1,110 @@
+export function dropdownFilter(data) {
+  const dropdownIngredients = document.getElementById("ingredientList");
+  const dropdownAppliance = document.getElementById("applianceList");
+  const dropdownUstensils = document.getElementById("ustensilsList");
+  //On créait des set pour vérifier que chaque élément de la liste est unique
+  const existingIngredients = new Set();
+  const existingAppliances = new Set();
+  const existingUstensils = new Set();
+
+  data.forEach((element) => {
+    element.ingredients.forEach((ingredientArray) => {
+      const ingredientName = ingredientArray.ingredient;
+
+      // Vérifier si l'ingrédient n'a pas déjà été ajouté
+      if (!existingIngredients.has(ingredientName)) {
+        const ingredientItemLi = document.createElement("li");
+        ingredientItemLi.innerHTML = ingredientName;
+        ingredientItemLi.setAttribute("class", "pl-4 py-2 cursor-pointer");
+        dropdownIngredients.appendChild(ingredientItemLi);
+
+        existingIngredients.add(ingredientName);
+      }
+    });
+
+    // ----------------------------------------------
+
+    const applianceName = element.appliance;
+    // Vérifier si l'appareil n'a pas déjà été ajouté
+    if (!existingAppliances.has(applianceName)) {
+      const applianceItemLi = document.createElement("li");
+      applianceItemLi.innerHTML = applianceName;
+      applianceItemLi.setAttribute("class", "pl-4 py-2 cursor-pointer");
+      dropdownAppliance.appendChild(applianceItemLi);
+
+      existingAppliances.add(applianceName);
+    }
+
+    // -----------------------------------------------
+    element.ustensils.forEach((ustensilArray) => {
+      const ustensilName = ustensilArray;
+
+      // Vérifier si l'ustensile n'a pas déjà été ajouté
+      if (!existingUstensils.has(ustensilName)) {
+        const ustensilItemLi = document.createElement("li");
+        ustensilItemLi.innerHTML = ustensilName;
+        ustensilItemLi.setAttribute("class", "pl-4 py-2 cursor-pointer");
+        dropdownUstensils.appendChild(ustensilItemLi);
+
+        existingUstensils.add(ustensilName);
+      }
+    });
+  });
+
+  dropdownIngredients.addEventListener("click", (event) => {
+    const ingredient = "ingredient"
+    if (event.target.tagName === 'LI') {
+      const filterActiveValue = event.target.getAttribute("filteractive");
+    
+      if (!filterActiveValue) {
+        const liValue = event.target.innerText
+        dropdownIngredients.removeChild(event.target)
+        filterItemDisplay(liValue, ingredient)
+        console.log(event.target)
+      }
+    }
+  })
+
+  dropdownAppliance.addEventListener("click", (event) => {
+    const appliance = "appliance"
+    if (event.target.tagName === 'LI') {
+      const filterActiveValue = event.target.getAttribute("filteractive");
+    
+      if (!filterActiveValue) {
+        const liValue = event.target.innerText
+        dropdownAppliance.removeChild(event.target)
+        filterItemDisplay(liValue, appliance)
+        console.log(event.target)
+      }
+    }
+  })
+
+  dropdownUstensils.addEventListener("click", (event) => {
+    const ustensils = "ustensils"
+    if (event.target.tagName === 'LI') {
+      const filterActiveValue = event.target.getAttribute("filteractive");
+    
+      if (!filterActiveValue) {
+        const liValue = event.target.innerText
+        dropdownUstensils.removeChild(event.target)
+        filterItemDisplay(liValue, ustensils)
+        console.log(event.target)
+      }
+    }
+  })
+}
+
+function filterItemDisplay(liValue, listType) {
+  const idList = listType + "Focus";
+  const focus = document.getElementById(idList);
+
+  // Vérifie si un élément avec le même ID existe déjà
+  if (!focus.querySelector(`[filterActive="${liValue}"]`)) {
+    const liElement = document.createElement("li");
+    liElement.setAttribute("filterActive", liValue);
+    liElement.setAttribute("class", "rounded-none pl-4 py-2 cursor-pointer");
+    liElement.innerText = liValue;
+
+    focus.appendChild(liElement);
+  }
+}
