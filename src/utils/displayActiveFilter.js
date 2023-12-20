19f14bbe89f8c1660137d9bb7ec811a1605fe119
gmaxdev <maxime.guigra@gmail.com>
2023-12-21 00:20:18 +0100

    update display filter active

--- src/utils/displayActiveFilter.js
+++ src/utils/displayActiveFilter.js
@@ -1,14 +1,21 @@
 import { dataRecipes } from "../../data/recipes";
 import { ModelRecipe } from "../models/ModelRecipe";
-import { ingredientList, applianceList, ustensilsList } from "./filterList";
+import {
+  filterList,
+  ingredientList,
+  applianceList,
+  ustensilsList,
+} from "./filterList";
 
 const recipeInstances = dataRecipes.map(
   (recipeData) => new ModelRecipe(recipeData)
 );
 
-// Affiche le tableau de tous les filtres dans une div
+// Affiche le tableau de tous les filtres dans une 'filterSpecificationZone'
 export function displayActiveFilter(data) {
-  const filterSpecificationZone = document.getElementById("filterSpecificationZone");
+  const filterSpecificationZone = document.getElementById(
+    "filterSpecificationZone"
+  );
   filterSpecificationZone.innerText = "";
 
   // Si data est un tableau, traite-le comme avant
@@ -17,43 +24,50 @@ export function displayActiveFilter(data) {
     elementBloc.innerHTML = `${element} <i class="cross cursor-pointer fa-solid fa-xmark"></i>`;
     elementBloc.setAttribute(
       "class",
-      "flex justify-between items-center h-14 w-52 mt-7 p-4 font-medium rounded-lg bg-amber-300 filterData"
+      "flex justify-between items-center h-14 w-52 mt-7 p-4 font-medium rounded-lg bg-amber-300"
     );
+
+    // elementBloc.querySelector(".cross").addEventListener("click", () => {
+    //   // Retirer l'élément spécifique de filterList
+    //   const indexToRemove = filterList.indexOf(elementBloc);
+    //   if (indexToRemove !== -1) {
+    //     filterList.splice(indexToRemove, 1);
+    //     filterSpecificationZone.removeChild(elementBloc);
+    //   }
+    // });
 
     // Nouvelle logique pour comparer avec le fichier JSON de recettes
     const matchType = findMatchType(element);
     console.log(`Correspondance pour ${element} : ${matchType}`);
-
     filterSpecificationZone.appendChild(elementBloc);
-    console.log(element)
   });
-  
-  // Reourne le type de l'élément
-  function findMatchType(element) {
-    const normalizedElement = element.toLowerCase();
-    for (const recipe of recipeInstances) {
-      // Vérifie les ingrédients
-      for (const ingredient of recipe.ingredients) {
-        if (ingredient.ingredient.toLowerCase() === normalizedElement) {
-          ingredientList.push(element);
-          return 'Ingredient';
-        }
-      }
-      // Vérifie l'appareil
-      if (recipe.appliance.toLowerCase() === normalizedElement) {
-        applianceList.push(element);
-        return 'Appliance';
-      }
+  // Aucune correspondance trouvée
+  return "Aucune correspondance";
+}
 
-      // Vérifie les ustensiles
-      for (const ustensil of recipe.ustensils) {
-        if (ustensil.toLowerCase() === normalizedElement) {
-          ustensilsList.push(element);
-          return 'Ustensil';
-        }
+// Reourne le type de l'élément
+function findMatchType(element) {
+  const normalizedElement = element.toLowerCase();
+  for (const recipe of recipeInstances) {
+    // Vérifie les ingrédients
+    for (const ingredient of recipe.ingredients) {
+      if (ingredient.ingredient.toLowerCase() === normalizedElement) {
+        ingredientList.push(element);
+        return "Ingredient";
       }
     }
+    // Vérifie l'appareil
+    if (recipe.appliance.toLowerCase() === normalizedElement) {
+      applianceList.push(element);
+      return "Appliance";
+    }
 
-    // Aucune correspondance trouvée
-    return 'Aucune correspondance';}
+    // Vérifie les ustensiles
+    for (const ustensil of recipe.ustensils) {
+      if (ustensil.toLowerCase() === normalizedElement) {
+        ustensilsList.push(element);
+        return "Ustensil";
+      }
+    }
+  }
 }
--- src/utils/dropdownFilter.js
+++ src/utils/dropdownFilter.js
@@ -1,38 +1,30 @@
 import { displayActiveFilter } from "../utils/displayActiveFilter";
 import { filterList } from "../utils/filterList";
 
+const dropdownIngredients = document.getElementById("ingredientList");
+const dropdownAppliance = document.getElementById("applianceList");
+const dropdownUstensils = document.getElementById("ustensilsList");
+
 // Ajoute des tableaux dans des listes de filtres
 export function dropdownFilter(data) {
-  const dropdownIngredients = document.getElementById("ingredientList");
-  const dropdownAppliance = document.getElementById("applianceList");
-  const dropdownUstensils = document.getElementById("ustensilsList");
-
-  dropdownIngredients.innerHTML = `<ul
-  id="ingredientFocus"
-  class="flex flex-col items-start h-auto focus listBlocFilter"
-></ul>`
-  dropdownAppliance.innerHTML = `<ul
-  id="applianceFocus"
-  class="flex flex-col items-start w-auto h-auto focus listBlocFilter bg-amber-300"
-></ul>`
-  dropdownUstensils.innerHTML = `<ul
-  id="ustensilsFocus"
-  class="flex flex-col items-start h-auto focus listBlocFilter"
-></ul>`
   //On créait des set pour vérifier que chaque élément de la liste est unique
   const existingIngredients = new Set();
   const existingAppliances = new Set();
   const existingUstensils = new Set();
 
   data.forEach((element) => {
+    // En fonction des du type de l'élément, on l'ajoute au dropdown correspondant
+
     element.ingredients.forEach((ingredientArray) => {
       const ingredientName = ingredientArray.ingredient;
-
       // Vérifier si l'ingrédient n'a pas déjà été ajouté
       if (!existingIngredients.has(ingredientName)) {
         const ingredientItemLi = document.createElement("li");
         ingredientItemLi.innerHTML = ingredientName;
-        ingredientItemLi.setAttribute("class", "pl-4 py-2 cursor-pointer ingredient");
+        ingredientItemLi.setAttribute(
+          "class",
+          "pl-4 py-2 cursor-pointer ingredient"
+        );
         dropdownIngredients.appendChild(ingredientItemLi);
 
         existingIngredients.add(ingredientName);
@@ -46,13 +38,17 @@ export function dropdownFilter(data) {
     if (!existingAppliances.has(applianceName)) {
       const applianceItemLi = document.createElement("li");
       applianceItemLi.innerHTML = applianceName;
-      applianceItemLi.setAttribute("class", "pl-4 py-2 cursor-pointer appliance");
+      applianceItemLi.setAttribute(
+        "class",
+        "pl-4 py-2 cursor-pointer appliance"
+      );
       dropdownAppliance.appendChild(applianceItemLi);
 
       existingAppliances.add(applianceName);
     }
 
     // -------------
+
     element.ustensils.forEach((ustensilArray) => {
       const ustensilName = ustensilArray;
 
@@ -60,7 +56,10 @@ export function dropdownFilter(data) {
       if (!existingUstensils.has(ustensilName)) {
         const ustensilItemLi = document.createElement("li");
         ustensilItemLi.innerHTML = ustensilName;
-        ustensilItemLi.setAttribute("class", "pl-4 py-2 cursor-pointer ustensil");
+        ustensilItemLi.setAttribute(
+          "class",
+          "pl-4 py-2 cursor-pointer ustensil"
+        );
         dropdownUstensils.appendChild(ustensilItemLi);
 
         existingUstensils.add(ustensilName);
@@ -70,45 +69,42 @@ export function dropdownFilter(data) {
 
   // -----------------------------------------------
 
-  dropdownIngredients.addEventListener("click", (event) => handleFilterItemClick(event, "ingredient"));
-  dropdownAppliance.addEventListener("click", (event) => handleFilterItemClick(event, "appliance"));
-  dropdownUstensils.addEventListener("click", (event) => handleFilterItemClick(event, "ustensils"));
-
+  // Si on clique sur un élément d'un dropdown, on lui modifie son état de focus
+  dropdownIngredients.addEventListener("click", (event) => {
+    handleFilterItemClick(event);
+    // dropdownIngredients.removeEventListener("click", (event))
+  });
+  dropdownAppliance.addEventListener("click", (event) => {
+    handleFilterItemClick(event);
+    // dropdownAppliance.removeEventListener("click", (event))
+  });
+  dropdownUstensils.addEventListener("click", (event) => {
+    handleFilterItemClick(event);
+    // dropdownUstensils.removeEventListener("click", (event))
+  });
 }
 
-function filterItemDisplay(liValue, listType) {
-  const idList = listType + "Focus";
-  const focus = document.getElementById(idList);
-
-  if (!focus.querySelector(`[filterActive="${liValue}"]`) && filterList.indexOf(liValue) === -1) {
-    // Ajoute à la liste focus
-    const liElement = document.createElement("li");
-    liElement.setAttribute("filterActive", liValue);
-    liElement.setAttribute("class", "flex justify-between items-center h-14 text-left rounded-none px-4 py-2 cursor-pointer bg-amber-400 w-full");
-    liElement.innerHTML = `${liValue} <i class="cross cursor-pointer fa-solid fa-xmark"></i>`;
-
-    liElement.querySelector(".cross").addEventListener("click", () => {
-      // Supprime de la liste focus et de la liste principale
-      liElement.removeAttribute('filterActive')
-    });
-
-    focus.appendChild(liElement);
-
-    // Ajoute à la liste principale
-    filterList.push(liValue);
-    displayActiveFilter(filterList);
+// On vérifie l'état de attribut 'filteractive'
+function handleFilterItemClick(event) {
+  const filterActiveValue = event.target.dataset.filteractive;
+  const liValue = event.target;
+  if (!filterActiveValue && liValue.nodeName === "LI") {
+    filterItemDisplay(liValue);
   }
 }
 
+function filterItemDisplay(liValue) {
+  // Ajoute à la liste focus
+  const parentList = liValue.parentElement;
 
-function handleFilterItemClick(event, listType) {
-  if (event.target.tagName === 'LI') {
-    const filterActiveValue = event.target.getAttribute("filteractive");
+  liValue.setAttribute("data-filteractive", "true");
+  liValue.setAttribute(
+    "class",
+    "flex justify-between items-center h-14 text-left rounded-none px-4 py-2 cursor-pointer bg-amber-400 w-full"
+  );
+  liValue.innerHTML += `<i class="cross cursor-pointer fa-solid fa-xmark"></i>`;
 
-    if (!filterActiveValue) {
-      const liValue = event.target.innerText;
-      event.target.remove();
-      filterItemDisplay(liValue, listType);
-    }
-  }
+  parentList.prepend(liValue); // Place l'élément focus en haut de la liste correspondante
+  filterList.push(liValue.innerText); // Ajoute à la liste principale si l'élément n
+  displayActiveFilter(filterList); // Affiche l'élément dans la zone de filtre globale
 }
