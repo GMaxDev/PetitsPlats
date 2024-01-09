aadca3430e459eb417cd0ee48318b8c5347d0014
gmaxdev <maxime.guigra@gmail.com>
2024-01-09 10:27:31 +0100

    Modif logique dropdown

--- src/models/RecipeManager.js
+++ src/models/RecipeManager.js
@@ -11,7 +11,7 @@ export class RecipeManager {
     );
 
     const arrayFilterResult = this._data.filter((recipe) => {
-      return Object.values(recipe).some((value) => {
+      const isSearchMatch = Object.values(recipe).some((value) => {
         let result = false;
         if (typeof value === "string") {
           // Si la propriété est une chaîne de caractères, vérifie si elle contient le filtre
@@ -25,94 +25,37 @@ export class RecipeManager {
         }
 
         // ----------------------------------------------
-
-        if(ingredientList.length > 0){
-          for(let i=0; i<ingredientList.length; i++){
-            if (typeof value === "string") {
-              result =  value.toLowerCase().includes(ingredientList[i].toLowerCase());
-            } else if (Array.isArray(value)) {
-              result = value.some(
-                (item) =>
-                  typeof item === "string" && item.toLowerCase().includes(ingredientList[i].toLowerCase())
-              );
-            }
-          }
-        }
-
-        if(applianceList.length > 0){
-          for(let i=0; i<applianceList.length; i++){
-            if (typeof value === "string") {
-              result =  value.toLowerCase().includes(applianceList[i].toLowerCase());
-            } else if (Array.isArray(value)) {
-              result = value.some(
-                (item) =>
-                  typeof item === "string" && item.toLowerCase().includes(applianceList[i].toLowerCase())
-              );
-            }
-          }
-        }
-
-
-        if(ustensilList.length > 0){
-          for(let i=0; i<ustensilList.length; i++){
-            if (typeof value === "string") {
-              result =  value.toLowerCase().includes(ustensilList[i].toLowerCase());
-            } else if (Array.isArray(value)) {
-              result = value.some(
-                (item) =>
-                  typeof item === "string" && item.toLowerCase().includes(ustensilList[i].toLowerCase())
-              );
-            }
-          }
-        }
         return result
       });
-    });
 
+      let ingredientSearch = ingredientList.length === 0
+      for(const selectedIngredient of ingredientList){
+        for (const recipeIngredient of recipe.ingredients){
+          ingredientSearch = ingredientSearch || recipeIngredient.ingredient.toLowerCase().includes(selectedIngredient.toLowerCase());
+        }
+      }
+
+      let applianceSearch = applianceList.length === 0
+      for(const selectedAppliance of applianceList){
+        applianceSearch = applianceSearch || recipe.appliance.toLowerCase() ===(selectedAppliance.toLowerCase());
+      }
+
+
+      let ustensilSearch = ustensilList.length === 0
+      for(const selectedUstensil of ustensilList){
+        for (const recipeUstensil of recipe.ustensils){
+          ustensilSearch = ustensilSearch || recipeUstensil.toLowerCase().includes(selectedUstensil.toLowerCase());
+        }
+      }
+
+      return isSearchMatch && ingredientSearch && applianceSearch && ustensilSearch
+    })
     // ----------------------------------------------
 
     this.recipeList = arrayFilterResult;
     console.log("Nouvelle liste: " + arrayFilterResult.length + " éléments");
     return this.recipeList;
   }
-
-  // recipeFilter(filter, tagList) {
-  //   console.log("Filtre tapé : "+filter+"; Liste des tags sélectionné : "+tagList)
-
-  //   const arrayFilterResult = this.recipeList.filter((recipe) => {
-  //     // Vérifie si le filtrce correspond à n'importe quelle propriété de la recette
-  //     return Object.values(recipe).some((value) => {
-  //       let result = false
-  //       if (typeof value === "string") {
-  //         // Si la propriété est une chaîne de caractères, vérifie si elle contient le filtre
-  //         result = value.toLowerCase().includes(filter);
-  //       } else if (Array.isArray(value)) {
-  //         // Si la propriété est un tableau, vérifie si l'un des éléments du tableau contient le filtre
-  //         result = value.some(
-  //           (item) =>
-  //             typeof item === "string" &&
-  //             item.toLowerCase().includes(filter)
-  //         );
-  //       }
-  //           // Vérifie si tous les termes de recherche sont présents dans la recette
-  //         // const termsFound = filterList.every(term => {
-  //         //   return Object.values(recipe).some(value => {
-  //         //     if (typeof value === 'string') {
-  //         //       return value.toLowerCase().includes(term.toLowerCase());
-  //         //     } else if (Array.isArray(value)) {
-  //         //       return value.some(item => typeof item === 'string' && item.toLowerCase().includes(term.toLowerCase()));
-  //         //     }
-  //         //     return false;
-  //         //   });
-  //         // });
-
-  //       return result;
-  //     });
-  //   });
-
-  //   this.recipeList = arrayFilterResult
-  //   return arrayFilterResult;
-  // }
 
   createRecipeCards() {
     const recipeCards = [];
