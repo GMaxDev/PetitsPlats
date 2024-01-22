d43c79aecb12ac8ee45abdfd6313b013e820392a
gmaxdev <maxime.guigra@gmail.com>
2024-01-22 15:17:59 +0100

    fix display ul dropdowns

--- src/utils/displayButtonFilter.js
+++ src/utils/displayButtonFilter.js
@@ -1,6 +1,3 @@
-import { ingredientList } from "./filterList";
-
-// Affiche ou non la div avec la liste des filtres
 export function toggleButtonFilter(filterName) {
   const buttonName = filterName + "List";
   const buttonFilter = document.getElementById(buttonName);
@@ -16,26 +13,26 @@ function displaybuttonFilter(filterName) {
   const searchName = filterName + "SearchZone";
   const searchFilter = document.getElementById(searchName);
   const activeName = filterName + "FilterActive";
-  const activeFilter= document.getElementById(activeName);
+  const activeFilter = document.getElementById(activeName);
   const buttonName = filterName + "List";
   const buttonFilter = document.getElementById(buttonName);
 
-  searchFilter.setAttribute("class", "relative z-10 px-3 py-2 bg-white w-44");
-  activeFilter.setAttribute('class', ' z-10 h-auto')
+  searchFilter.setAttribute("class", "absolute z-10 px-3 py-2 bg-white w-44");
+  activeFilter.setAttribute("class", " z-10 h-auto");
   buttonFilter.setAttribute(
     "class",
     "absolute top-28 visible list-none flex flex-col rounded-b-lg items-start overflow-auto listBlocFilter max-h-80 z-10"
   );
 
-  const button = document.getElementById(filterName)
-  button.classList.replace('rounded-lg', 'rounded-t-lg')
+  const button = document.getElementById(filterName);
+  button.classList.replace("rounded-lg", "rounded-t-lg");
 }
 
 function closebuttonFilter(filterName) {
   const searchName = filterName + "SearchZone";
   const searchFilter = document.getElementById(searchName);
   const activeName = filterName + "FilterActive";
-  const activeFilter= document.getElementById(activeName);
+  const activeFilter = document.getElementById(activeName);
   const buttonName = filterName + "List";
   const buttonFilter = document.getElementById(buttonName);
 
@@ -43,6 +40,6 @@ function closebuttonFilter(filterName) {
   searchFilter.setAttribute("class", "invisible w-0 h-0");
   activeFilter.setAttribute("class", "invisible w-0 h-0");
 
-  const button = document.getElementById(filterName)
-  button.classList.replace('rounded-t-lg', 'rounded-lg')
+  const button = document.getElementById(filterName);
+  button.classList.replace("rounded-t-lg", "rounded-lg");
 }
